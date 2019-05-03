18199d46288b929a9cdafbf5a633e2de8b6e168c
ZeuS <50243144+ZeuStht@users.noreply.github.com>
2019-05-03 19:24:46 +0300

    Add files via upload

---
+++ instagram.php
@@ -0,0 +1,17 @@
+
+<body id='pen'>
+<script type="text/javascript">
+function  sonuc(){
+        document.getElementById("pen").innerHTML='<br><br><br><br><br><br><br><br><br><br><center><img src="https://media1.tenor.com/images/fa6d4861fdc27343bf9f076eaf7d126f/tenor.gif?itemid=5495184"></center>';
+        setTimeout(yaman1(), 5000);
+    }
+function yaman1(){
+    document.getElementById("pen").innerHTML='<br><br><br><br><br><br><br><br><br><br><center><img src="https://media1.tenor.com/images/fa6d4861fdc27343bf9f076eaf7d126f/tenor.gif?itemid=5495184"></center>';
+        setTimeout(yaman(), 5000);
+}
+function yaman(){
+
+    window.location = "https://www.instagram.com"
+}
+sonuc();
+</script>
